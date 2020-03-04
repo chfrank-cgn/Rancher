1b4242f6b95a50e4d73b84c889d1795e46dc11ab
Christian Frank <42704306+chfrank-cgn@users.noreply.github.com>
2020-03-04 11:48:31 +0100

    Azure Node Driver

---
+++ az-cluster-1/main.tf
@@ -0,0 +1,125 @@
+# Terraform resources 
+
+# Random ID
+resource "random_id" "instance_id" {
+ byte_length = 3
+}
+
+# Azure Cloud Credentials
+resource "rancher2_cloud_credential" "credential_az" {
+  name = "Free Trial"
+  azure_credential_config {
+    client_id = var.az-client-id
+    client_secret = var.az-client-secret
+    subscription_id = var.az-subscription-id
+  }
+}
+
+# Azure Node Template
+resource "rancher2_node_template" "template_az" {
+  name = "RKE Node Template"
+  cloud_credential_id = rancher2_cloud_credential.credential_az.id
+  engine_install_url = var.dockerurl
+  azure_config {
+    disk_size = var.disksize
+    image = var.image
+    location = var.az-region
+    managed_disks = true
+    no_public_ip = false
+    open_port = var.az-portlist
+    resource_group = var.az-resource-group
+    storage_type = var.az-storage-type
+    size = var.type
+    use_private_ip = false
+  }
+}
+
+# Rancher cluster
+resource "rancher2_cluster" "cluster_az" {
+  name         = "az-${random_id.instance_id.hex}"
+  description  = "Terraform"
+
+  rke_config {
+    kubernetes_version = var.k8version
+    ignore_docker_version = false
+    cloud_provider {
+      name = "azure"
+      azure_cloud_provider {
+        aad_client_id = var.az-client-id
+        aad_client_secret = var.az-client-secret
+        subscription_id = var.az-subscription-id
+        tenant_id = var.az-tenant-id
+        resource_group = var.az-resource-group
+      }
+    }
+    network {
+      plugin = "flannel"
+    }
+    services { 
+      etcd {
+        backup_config {
+          enabled = false
+        }
+      }
+      kubelet {
+        extra_args  = {
+          max_pods = 70
+        }
+      }
+    }
+  }
+}
+
+# Azure Node Pool
+resource "rancher2_node_pool" "nodepool_az" {
+  cluster_id = rancher2_cluster.cluster_az.id
+  name = "nodepool"
+  hostname_prefix = "rke-${random_id.instance_id.hex}-"
+  node_template_id = rancher2_node_template.template_az.id
+  quantity = var.numnodes
+  control_plane = true
+  etcd = true
+  worker = true
+}
+
+# Delay hack part 1
+resource "null_resource" "before" {
+  depends_on = [rancher2_cluster.cluster_az]
+}
+
+# Delay hack part 2
+resource "null_resource" "delay" {
+  provisioner "local-exec" {
+    command = "sleep ${var.delaysec}"
+  }
+
+  triggers = {
+    "before" = "${null_resource.before.id}"
+  }
+}
+
+# Kubeconfig file
+resource "local_file" "kubeconfig" {
+  filename = "${path.module}/.kube/config"
+  content = rancher2_cluster.cluster_az.kube_config
+  file_permission = "0600"
+
+  depends_on = [null_resource.delay]
+}
+
+# Cluster logging
+resource "rancher2_cluster_logging" "az_syslog" {
+  name = "az_syslog"
+  cluster_id = rancher2_cluster.cluster_az.id
+  kind = "syslog"
+  syslog_config {
+    endpoint = "rancher.chfrank.net:514"
+    protocol = "udp"
+    program = "az-${random_id.instance_id.hex}"
+    severity = "notice"
+    ssl_verify = false
+  }
+
+  depends_on = [local_file.kubeconfig]
+}
+
---
+++ az-cluster-1/variables.tf
@@ -0,0 +1,70 @@
+# Input variables
+
+# Node image
+variable "image" { 
+	default = "canonical:UbuntuServer:18.04-LTS:latest"
+}
+
+# Node disk size in GB
+variable "disksize" {
+	default = 30
+}
+
+# Node type
+variable "type" {
+	default = "Standard_D2s_v3"
+}
+
+# Kubernetes version
+variable "k8version" {
+	default = "v1.15.10-rancher1-1"
+}
+
+# Docker version
+variable "dockerurl" {
+	default = "https://releases.rancher.com/install-docker/18.06.sh"
+}
+
+# Number of nodes
+variable "numnodes" {
+	default = 3
+}
+
+# Azure open ports
+variable "az-portlist" {
+	type = list(string)
+        default = ["80/tcp","443/tcp","6443/tcp","2379/tcp","2380/tcp","8472/udp","4789/udp","9796/tcp","10256/tcp","10250/tcp","10251/tcp","10252/tcp"]
+}
+
+# Azure Resource Group
+variable "az-resource-group" {
+	default = "az-cluster-1"
+}
+
+# Azure Region
+variable "az-region" {
+	default = "westeurope"
+}
+
+# Azure Storage Type
+variable "az-storage-type" {
+	default = "Premium_LRS"
+}
+
+# Hack: Time to wait for Kubernetes to deploy
+variable "delaysec" {
+	default = 1020
+}
+
+variable "rancher-url" { }
+
+variable "rancher-token" { }
+
+variable "az-client-id" { }
+
+variable "az-client-secret" { }
+
+variable "az-subscription-id" { }
+
+variable "az-tenant-id" { }
+

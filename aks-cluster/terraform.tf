2b426e3c103a1efc0e25608957535f76f6acdf12
Christian Frank <42704306+chfrank-cgn@users.noreply.github.com>
2023-10-13 20:33:56 +0200

    Grafana 10.5.1

--- aks-cluster/terraform.tf
+++ aks-cluster/terraform.tf
@@ -28,7 +28,7 @@ terraform {
     }
     rancher2 = {
       source  = "rancher/rancher2"
-      version = "~> 3.1"
+      version = "~> 3.2"
     }
     random = {
       source  = "hashicorp/random"

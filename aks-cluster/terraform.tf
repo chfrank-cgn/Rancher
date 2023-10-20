ddbdd2e07f2fff9fd11b62c92029c2b5b981715c
Christian Frank <42704306+chfrank-cgn@users.noreply.github.com>
2023-10-20 16:37:29 +0200

    Self-metrics

--- aks-cluster/terraform.tf
+++ aks-cluster/terraform.tf
@@ -12,7 +12,7 @@ terraform {
   required_providers {
     azurerm = {
       source  = "hashicorp/azurerm"
-      version = "~> 3.76"
+      version = "~> 3.77"
     }
     kubernetes = {
       source = "hashicorp/kubernetes"

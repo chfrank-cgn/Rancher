e2a328536af7f3fba408f21d3b5675ae50a7044d
Christian Frank <42704306+chfrank-cgn@users.noreply.github.com>
2024-04-12 20:14:09 +0200

    terraform 1.8

--- aks-cluster/terraform.tf
+++ aks-cluster/terraform.tf
@@ -12,7 +12,7 @@ terraform {
   required_providers {
     azurerm = {
       source  = "hashicorp/azurerm"
-      version = "~> 3.98"
+      version = "~> 3.99"
     }
     kubernetes = {
       source = "hashicorp/kubernetes"
--- aks-cluster/versions.tf
+++ aks-cluster/versions.tf
@@ -1,3 +1,3 @@
 terraform {
-  required_version = ">= 1.7"
+  required_version = ">= 1.8"
 }

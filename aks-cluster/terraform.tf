ac61463262ff51bad64a6f801c6856425a7dc90a
Christian Frank <42704306+chfrank-cgn@users.noreply.github.com>
2026-03-29 23:34:39 +0200

    Update terraform.tf

--- aks-cluster/terraform.tf
+++ aks-cluster/terraform.tf
@@ -12,7 +12,7 @@ terraform {
   required_providers {
     azurerm = {
       source  = "hashicorp/azurerm"
-      version = "~> 4.61"
+      version = "~> 4.66"
     }
     kubernetes = {
       source = "hashicorp/kubernetes"
